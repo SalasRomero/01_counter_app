996c96b663f3766d44b97e50bae91e570e41300c
David Salas <dasa.19983@gmail.com>
2024-09-16 21:48:34 -0500

    Funcionalidad App

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public n:number;
+  public showNumber:string;
+
+  constructor() {
+    this.n = 0;
+    this.showNumber = "00";
+  }
+
+
+  up():void{
+    this.n = this.n + 1;
+    this.formatShowHNumber();
+  }
+
+  down():void{
+    this.n = this.n - 1;
+    this.formatShowHNumber();
+
+  }
+
+  formatShowHNumber(){
+    if(this.n < 10){
+      this.showNumber = '0'+this.n;
+    }else{
+      this.showNumber = this.n.toString();
+    }
+  }
 
 }
